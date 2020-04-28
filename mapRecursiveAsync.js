0b70a071ad6df373494d49be8d88b61f0972d6d1
aetonsi <18366087+aetonsi@users.noreply.github.com>
2020-04-28 19:38:51 +0200

    refactoring
    * added default value for isObject param
    * formatting

--- mapRecursiveAsync.js
+++ mapRecursiveAsync.js
@@ -1,21 +1,21 @@
-const mapRecursiveAsync = async (object, callback, isObjectOverride = null) => {
-  // isObject alternatives: lodash.isPlainObject, jonschlinkert/is-plain-object
-  const isObject = !!isObjectOverride ? isObjectOverride : object => Object.prototype.toString.call(object) === '[object Object]';
-  return await (async function worker(something, callback, key, object) {
-    let result;
-    if (Array.isArray(something)) {
-      result = [];
-      for (let i = 0; i < something.length; i++) {
-        result[i] = await worker(something[i], callback, i, something);
-      }
-    } else if (isObject(something)) {
-      result = {};
-      for (let k in something) {
-        result[k] = await worker(something[k], callback, k, something);
-      }
-    } else {
-      result = await callback(something, key, object);
+const mapRecursiveAsync = async (
+  object,
+  callback,
+  isObject = object => Object.prototype.toString.call(object) === '[object Object]' // alternatives: lodash.isPlainObject, jonschlinkert/is-plain-object
+) => await (async function worker(something, callback, key, object) {
+  let result;
+  if (Array.isArray(something)) {
+    result = [];
+    for (let i = 0; i < something.length; i++) {
+      result[i] = await worker(something[i], callback, [...key, i], something);
     }
-    return result;
-  })(object, callback, void 0, object);
-};
+  } else if (isObject(something)) {
+    result = {};
+    for (let k in something) {
+      result[k] = await worker(something[k], callback, [...key, k], something);
+    }
+  } else {
+    result = await callback(something, key, object);
+  }
+  return result;
+})(object, callback, [], object);
