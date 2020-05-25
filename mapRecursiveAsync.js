25058f2425579ee02a74249fca4cc1e97b59c6a0
aetonsi <18366087+aetonsi@users.noreply.github.com>
2020-05-25 21:28:50 +0200

    arguments refactoring & es6 export & docblock
    cleaned up arguments
    added es6 export
    added docblock

--- mapRecursiveAsync.js
+++ mapRecursiveAsync.js
@@ -1,21 +1,34 @@
-const mapRecursiveAsync = async (
+/**
+ *
+ * @param {Object} object
+ * @param {AsyncFunction(value, key, object, keyPath)} callback
+ * @returns AsyncFunction
+ */
+export const mapRecursiveAsync = async (
   object,
   callback,
   isObject = object => Object.prototype.toString.call(object) === '[object Object]' // alternatives: lodash.isPlainObject, jonschlinkert/is-plain-object
-) => await (async function worker(something, callback, key, object) {
-  let result;
-  if (Array.isArray(something)) {
-    result = [];
-    for (let i = 0; i < something.length; i++) {
-      result[i] = await worker(something[i], callback, [...key, i], something);
+) => (await (
+  async function worker(something, keyPath) {
+    let result;
+    if (Array.isArray(something)) { // recurse
+      result = [];
+      for (let i = 0; i < something.length; i++) {
+        result[i] = await worker(something[i], [...keyPath, i]);
+      }
+    } else if (isObject(something)) { // recurse
+      result = {};
+      for (let k in something) {
+        result[k] = await worker(something[k], [...keyPath, k]);
+      }
+    } else { // callback
+      result = await callback(
+        something,
+        keyPath[keyPath.length - 1],
+        object,
+        keyPath
+      );
     }
-  } else if (isObject(something)) {
-    result = {};
-    for (let k in something) {
-      result[k] = await worker(something[k], callback, [...key, k], something);
-    }
-  } else {
-    result = await callback(something, key, object);
+    return result;
   }
-  return result;
-})(object, callback, [], object);
+)(object, []));
